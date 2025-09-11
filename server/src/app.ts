87c263a053f4b5974467fa76f3ddf5e4f3c83eb4
m-a-x-c <35903396+m-a-x-c@users.noreply.github.com>
2025-09-11 21:55:00 +0800

    feat: add CORS and morgan request logging

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
+import cors from 'cors';
 import express, { Application } from 'express';
+import morgan from 'morgan';
 import notFound from './middlewares/notFound';
 import globalErrorHandler from './middlewares/globalErrorhandler';
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'] }));
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Inventory Management System API' });
