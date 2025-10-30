1cb5acf70dfbc2d63ad143d7f8c05ef3514dc230
m-a-x-c <35903396+m-a-x-c@users.noreply.github.com>
2025-10-30 22:19:00 +0800

    feat: seller backend module (model, services, routes)

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRoutes from '../modules/user/user.routes';
+import sellerRoutes from '../modules/seller/seller.routes';
 
 const rootRouter = Router();
 
 rootRouter.use('/users', userRoutes);
+rootRouter.use('/sellers', sellerRoutes);
 
 export default rootRouter;
