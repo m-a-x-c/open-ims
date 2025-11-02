1d7d7a0629a985412fe904d0aa29ee54370d79ef
m-a-x-c <35903396+m-a-x-c@users.noreply.github.com>
2025-11-02 19:46:00 +0800

    feat: category and brand backend modules

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import userRoutes from '../modules/user/user.routes';
 import sellerRoutes from '../modules/seller/seller.routes';
+import categoryRoutes from '../modules/category/category.routes';
+import brandRoutes from '../modules/brand/brand.routes';
 
 const rootRouter = Router();
 
 rootRouter.use('/users', userRoutes);
 rootRouter.use('/sellers', sellerRoutes);
+rootRouter.use('/categories', categoryRoutes);
+rootRouter.use('/brands', brandRoutes);
 
 export default rootRouter;
